4385172cf4442158600bcb400ea527e6bda6f76a
surflip <surflip@163.com>
2024-03-27 19:42:13 +0800

    Update stinstall.sh

--- stinstall.sh
+++ stinstall.sh
@@ -3,5 +3,5 @@ cd ~
 pkg install git -y
 pkg install nodejs -y
 git clone https://github.com/SillyTavern/SillyTavern.git
-npm config set registry?https://registry.npmmirror.com
+npm config set registry https://registry.npmmirror.com
 cd ~/SillyTavern/ && echo y | sh ./start.sh
